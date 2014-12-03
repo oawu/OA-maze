926ceb59cc58490e9fb7034249c89480d2e51913
OA Wu <comdan66@gmail.com>
2014-12-03 21:36:36 +0800

    Sample2

--- res/index.js
+++ res/index.js
@@ -12,10 +12,6 @@ ga('create', 'UA-46121102-7', 'auto');
 ga('send', 'pageview');
 
 $(function () {
-
-  var checkMaze = function (maze) {
-    return true;
-  }
   var maze = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
@@ -36,28 +32,46 @@ $(function () {
     [0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   ];
-  var dimension = 20;
-  var cD = 50;
-  var startPoint = {x: 1, y: 1};
-  var endPoint = {x: 19, y: 13};
+
+  var checkMaze = function (maze) {
+    return true;
+  }
+
+  if (!checkMaze ()) return;
+  
+  var classMap = ['wall', 'road', 'now', 'end'];
+
+  var dimension = 20, column = maze[0].length, row = maze.length;
+  var $view = $('#view');
+  var $units = $('#units').css ({'width': column * dimension + 'px', 'height': row * dimension + 'px'})
+                          .append (
+                            maze.map (function (t) {
+                              return t.map (function (u) {
+                                return $('<div />').css ({'width': dimension + 'px', 'height': dimension + 'px'})
+                                                   .addClass ('unit')
+                                                   .addClass (classMap[u]);
+                              });
+                            }).reduce (function (p, n) { return p.concat (n) })
+                          );
+
+  var cW = parseFloat ($view.width ()) * 2;
+  var cH = parseFloat ($view.height ()) * 2;
+  var cD = dimension * 3;
+  var $cover = $('<div />').css ({'top': 0 - ($view.height () / 2) + 'px','left': 0 - ($view.width () / 2) + 'px', 'width': cW + 'px', 'height': cH + 'px', 'line-height': cH + 'px'})
+                           .addClass ('cover')
+                           .append (
+                              $('<div />').css ({'width': cD + 'px', 'height': cD + 'px', 'box-shadow': '0 0 0 ' + (Math.max (cW, cH) * 1.5 - cD) / 2 + 'px rgba(0, 0, 0, 1), inset 0 0 10px 3px rgba(0, 0, 0, 1)'})
+                                          .addClass ('round')
+                            )
+                           .appendTo ($view);
+
+  var startPoint = {x: 5, y: 3, type: 2};
+  var endPoint = {x: 19, y: 13, type: 3};
   var nowPoint = {};
-  var column = 0, row = 0;
-  var classMap = ['wall', 'road', 'now', 'end']
-  if (!checkMaze (maze)) return;
-  column = maze[0].length;
-  row = maze.length;
 
-  var cW = (column * dimension) * 2;
-  var cH = (row * dimension) * 2;
-  var $cover = $('<div />').addClass ('cover').css ({'width': cW + 'px', 'height': cH + 'px', 'line-height': cH + 'px'}).append ($('<div />').addClass ('round').css ({'width': cD + 'px', 'height': cD + 'px', 'box-shadow': '0 0 0 ' + (Math.max (cW, cH) * 1.5 - cD) / 2 + 'px rgba(0, 0, 0, 1), inset 0 0 10px 3px rgba(0, 0, 0, 1)'}));
-  $('#units').css ({'width': column * dimension + 2 + 'px', 'height': row * dimension + 2 + 'px'});
-  $('#units').append (maze.map (function (t) { return t.map (function (u) { return $('<div />').addClass ('unit').css ({'width': dimension + 'px', 'height': dimension + 'px'}).addClass (classMap[u]); }); }).reduce (function (p, n) { return p.concat (n) }))
-  $("#units").append ($cover);
-
-  var printR = function () {
-    $.each (maze, function (i, t) {
-      console.info (t.join (''));
-      console.info ();
+  var fetchMaze = function (diff) {
+    $.each (diff, function (i, t) {
+      $('#units .unit').eq (t.x + t.y * column).removeClass().addClass ('unit').addClass (classMap [t.type])
     });
   }
   var diffMaze = function (n, o) {
@@ -67,50 +81,24 @@ $(function () {
       });
     }).reduce (function (p, q) { return p.concat (q) }).filter (function (t) { return t; });
   }
-  var fetchMaze = function (diff) {
-    $.each (diff, function (i, t) {
-      $('#units .unit').eq (t.x + t.y * column).removeClass().addClass ('unit').addClass (classMap [t.type])
-    });
+  var moveMaze = function () {
+    cW = parseFloat ($view.width ()) * 2;
+    cH = parseFloat ($view.height ()) * 2;
+    $units.css ({'top': ($view.height () / 2) - ((nowPoint.y + 0.5) * dimension) + 'px', 'left': ($view.width () / 2) - ((nowPoint.x + 0.5) * dimension) + 'px'});
+    $cover.css ({'top': 0 - ($view.height () / 2) + 'px','left': 0 - ($view.width () / 2) + 'px', 'width': cW + 'px', 'height': cH + 'px', 'line-height': cH + 'px'});
   }
-  var setCover = function () {
-    var x = nowPoint.x * dimension + dimension / 2;
-    var y = nowPoint.y * dimension + dimension / 2;
-    var t = 0 - (cH / 2);
-    var l = 0 - (cW / 2);
-    $cover.css ({top: t + y + 'px', left: l + x + 'px'})
-  }
-  var setStartEnd = function (startPoint, endPoint) {
-    var newMaze = maze.map (function (u) { return u.slice (0); })
-    newMaze[startPoint.y][startPoint.x] = 2;
-    newMaze[endPoint.y][endPoint.x] = 3;
-    var diff = diffMaze (newMaze, maze);
-    fetchMaze (diff);
-    nowPoint = startPoint;
-    maze = newMaze;
-    setCover ();
-  }
-  var run = function (s, e) {
+  var run = function (s, e, n) {
     var newMaze = maze.map (function (u) { return u.slice (0); })
     newMaze[s.y][s.x] = s.type;
     newMaze[e.y][e.x] = e.type;
     var diff = diffMaze (newMaze, maze);
     fetchMaze (diff);
-    nowPoint = e;
+    nowPoint = n ? s : e;
+    moveMaze ();
     maze = newMaze;
-    setCover ();
   }
-  var addRule = (function(style){
-      var sheet = document.head.appendChild(style).sheet;
-      return function(selector, css){
-          var propText = Object.keys(css).map(function(p){
-              return p+":"+css[p]
-          }).join(";");
-          sheet.insertRule(selector + "{" + propText + "}", sheet.cssRules.length);
-      }
-  })(document.createElement("style"));
 
-
-  setStartEnd (startPoint, endPoint);
+  run (startPoint, endPoint, 1);
 
   $(window).on ('keydown', function (e) {
     if (e.keyCode == 38) {
@@ -142,6 +130,4 @@ $(function () {
       }
     }
   })
-
-
 });
